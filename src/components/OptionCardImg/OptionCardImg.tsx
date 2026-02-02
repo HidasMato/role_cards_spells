81703f14c87021fa9750e50cdc3f7db163701471
HidasMato <hidasmato@gmail.com>
2026-02-02 21:18:18 +0300

    Добавлена база 24 днд. Спаршена с сайта new.ttg.club. Не проверена

--- src/components/OptionCardImg/OptionCardImg.tsx
+++ src/components/OptionCardImg/OptionCardImg.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import style from './OptionCardImg.module.scss';
 type AlertProps = {
-    // nameSetting: string;
-    // children: JSX.Element;
     targetThis: boolean;
     setTarget: (a: number) => void;
-    number: number;
+    key: number;
     img?: string;
     name: string;
     children?: JSX.Element;
     d?: boolean;
     numHide?: boolean;
 };
-const OptionCardImg = ({numHide, d, img, name, targetThis, setTarget, number, children }: AlertProps): JSX.Element => {
+const OptionCardImg = ({numHide, d, img, name, targetThis, setTarget, key, children }: AlertProps): JSX.Element => {
     return (
         <div>
-            <div className={style.NameDownload}> {numHide ? '' :  number +'.' }{name} {(d) ? children : null} </div>
-            <div className={style.Main + ' ' + (targetThis ? style.Target : '')} onClick={(e) => {  setTarget(number); }} >
-                {img != undefined  ? <img src={img} alt="" /> : null}
-                {children != undefined  && !d ? <div className={style.Children}>{children}</div> : null}
+            <div className={style.NameDownload}> {numHide ? '' :  key +'.' }{name} {(d) ? children : null} </div>
+            <div className={style.Main + ' ' + (targetThis ? style.Target : '')} onClick={(e) => {  setTarget(key); }} >
+                {img !== undefined  ? <img src={img} alt="" /> : null}
+                {children !== undefined  && !d ? <div className={style.Children}>{children}</div> : null}
             </div>
         </div>
     );
---
+++ src/spellBase/spellBaseType.ts
@@ -0,0 +1,13 @@
+type spellBaseType = {
+    0: string[],
+    1: string[],
+    2: string[],
+    3: string[],
+    4: string[],
+    5: string[],
+    6: string[],
+    7: string[],
+    8: string[],
+    9: string[],
+};
+export default spellBaseType;
